1a66532302a7a5497d92a95e9dfe0b5170b5ae34
NaufalArk213 <naufalarkana0403@gmail.com>
2026-09-08 23:27:19 +0700

    Fix output formatting and battery logic in Problem2.cpp

--- Problem2.cpp
+++ Problem2.cpp
@@ -3,16 +3,12 @@ using namespace std;
 
 int main() {
     int n, C, R, K;
-
     cout << "Masukkan jumlah misi (n): ";
     cin >> n;
-
     cout << "Masukkan kapasitas maksimum baterai (C): ";
     cin >> C;
-
     cout << "Masukkan penambahan energi sekali charge (R): ";
     cin >> R;
-
     cout << "Masukkan batas maksimal kali charge (K): ";
     cin >> K;
 
@@ -22,25 +18,19 @@ int main() {
 
     for (int i = 0; i < n; i++) {
         int x;
-        cout << "Kebutuhan energi misi ke-" << (i + 1) << ": ";
-        cin >> x; 
-
-        // Charging jika baterai kurang dan masih ada sisa kuota K
+        cout << "Kebutuhan energi misi ke " << (i + 1) << ": ";
+        cin >> x;  
         while (baterai < x && k_sisa > 0) {
             baterai += R;
             if (baterai > C) {
-                baterai = C; // Baterai maksimal sebatas kapasitas C
+                baterai = C; 
             }
             k_sisa--;
-        }
-
-        // Jalankan misi jika baterai mencukupi
-        if (baterai >= x) {
+        }if (baterai >= x) {
             baterai -= x;
             misi_selesai++;
         } else {
-            // Berhenti jika baterai tidak cukup
-            cout << "Baterai tidak cukup untuk melanjutkan misi ke-" << (i + 1) << "!" << endl;
+            cout << "Baterai tidak cukup untuk melanjutkan misi ke " << (i + 1) << endl;
             break;
         }
     }
